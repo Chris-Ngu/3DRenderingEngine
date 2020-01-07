7f5c7c4fa7c5b902a72a2171f3d16fde6ce2aede
Christopher Nguyen <Chris_ngu@pm.me>
2020-01-07 16:13:14 -0600

    Adding camera lighting

--- C+GameEngine.cpp
+++ C+GameEngine.cpp
@@ -9,6 +9,9 @@ struct triangle
 {
     //triangle consists of three points
     vec3d p[3]; 
+
+    wchar_t sym;
+    short col;
 };
 struct mesh
 {
@@ -23,7 +26,7 @@ struct mat4x4
 
 class olcEngine3D : public olcConsoleGameEngine
 {
-public :
+public:
     olcEngine3D()
     {
         m_sAppName = L"3D DEMO";
@@ -32,9 +35,9 @@ private:
     mesh meshCube;
     mat4x4 matProj;
     float fTheta;
-    vec3d vCamera;  
+    vec3d vCamera;
 
-    void MultiplyMatrixVector(vec3d &i, vec3d &o, mat4x4 &m)
+    void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m)
     {
         o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
         o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
@@ -46,6 +49,41 @@ private:
             o.x /= w; o.y /= w; o.z /= w;
         }
     }
+
+	CHAR_INFO GetColour(float lum)
+	{
+		short bg_col, fg_col;
+		wchar_t sym;
+		int pixel_bw = (int)(13.0f*lum);
+		switch (pixel_bw)
+		{
+            //Black and white shading
+		    case 0: bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID; break;
+
+		    case 1: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_QUARTER; break;
+		    case 2: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_HALF; break;
+		    case 3: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_THREEQUARTERS; break;
+		    case 4: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_SOLID; break;
+
+		    case 5: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_QUARTER; break;
+		    case 6: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_HALF; break;
+		    case 7: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_THREEQUARTERS; break;
+		    case 8: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_SOLID; break;
+
+		    case 9:  bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_QUARTER; break;
+		    case 10: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_HALF; break;
+		    case 11: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_THREEQUARTERS; break;
+		    case 12: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_SOLID; break;
+		    default: bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID;
+			
+		}
+
+		CHAR_INFO c;
+		c.Attributes = bg_col | fg_col;
+		c.Char.UnicodeChar = sym;
+		return c;
+	}
+
 
 public:
     bool OnUserCreate() override
@@ -73,11 +111,11 @@ public:
             {1.0f, 0.0f, 1.0f,  0.0f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f},
         };
         // Projction matrix
-		float fNear = 0.1f;
-		float fFar = 1000.0f;
-		float fFov = 90.0f;
-		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
-		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+        float fNear = 0.1f;
+        float fFar = 1000.0f;
+        float fFov = 90.0f;
+        float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+        float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
 
         matProj.m[0][0] = fAspectRatio * fFovRad;
         matProj.m[1][1] = fFovRad;
@@ -144,13 +182,26 @@ public:
 
             //3d -> 2d
             //if (normal.z < 0) 
-                if (normal.x * (triTranslated.p[0].x - vCamera.x)+
-                    normal.y * (triTranslated.p[0].y - vCamera.y)+
-                    normal.z * (triTranslated.p[0].z - vCamera.z) < 0) //Camera perspective view of blocking triangles
+            if (normal.x * (triTranslated.p[0].x - vCamera.x)+
+                normal.y * (triTranslated.p[0].y - vCamera.y)+
+                normal.z * (triTranslated.p[0].z - vCamera.z) < 0) //Camera perspective view of blocking triangles
             { 
+                //lighting
+                vec3d light_direction = { 0.0f, 0.0f, -1.0f };
+                float l = sqrtf(light_direction.x * light_direction.x + light_direction.y * light_direction.y + light_direction.z * light_direction.z);
+                light_direction.x /= l; light_direction.y /= l; light_direction.z /= l;
+
+                float dp = normal.x * light_direction.x + normal.y * light_direction.y + normal.z * light_direction.z;
+                CHAR_INFO c = GetColour(dp);
+                triTranslated.col = c.Attributes;
+                triTranslated.sym = c.Char.UnicodeChar;
+
+                //Triangles 3d -> 2d
                 MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
                 MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
                 MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+                triProjected.col = triTranslated.col;
+                triProjected.sym = triTranslated.sym;
 
                 //Scaling (something is wrong here and not scaling correctly, fps is super low)
                 triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
@@ -166,10 +217,15 @@ public:
                 triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
 
 
-                DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                FillTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                triProjected.p[1].x, triProjected.p[1].y,
+                triProjected.p[2].x, triProjected.p[2].y,
+                triProjected.sym, triProjected.col);
+
+                /*DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
                     triProjected.p[1].x, triProjected.p[1].y,
                     triProjected.p[2].x, triProjected.p[2].y,
-                    PIXEL_SOLID, FG_WHITE);
+                    PIXEL_SOLID, FG_WHITE);*/
             }
         }
 
