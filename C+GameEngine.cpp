e0aa14e9292b0521be2240d5c88a72bdfc8d295f
Christopher Nguyen <Chris_ngu@pm.me>
2020-01-10 17:22:10 -0600

    bugfixing textures

--- C+GameEngine.cpp
+++ C+GameEngine.cpp
@@ -81,6 +81,7 @@ private:
     vec3d vLookDir;
     float fTheta;
     float fYaw;
+    olcSprite* sprTex1;
 
     vec3d Matrix_MultiplyVector(mat4x4 &m, vec3d &i)
     {
@@ -416,6 +417,7 @@ public:
             { 1.0f, 0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 0.0f, 1.0f,    1.0f, 0.0f, 0.0f, 1.0f,		0.0f, 1.0f, 1.0f,		1.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f,},
             
         };
+        sprTex1 = new olcSprite(L"minijario.spr");
 
         matProj = Matrix_MakeProjection(90.0f, (float)ScreenHeight() / (float)ScreenWidth(), 0.1f, 1000.0f);
         return true;
@@ -591,6 +593,9 @@ public:
 
             for (auto& t : listTriangles)
             {
+                TexturedTriangle(t.p[0].x, t.p[0].y, t.t[0].u, t.t[0].v,
+                    t.p[1].x, t.p[1].y, t.t[1].u, t.t[1].v,
+                    t.p[2].x, t.p[2].y, t.t[2].u, t.t[2].v, sprTex1);
                 //FillTriangle(t.p[0].x, t.p[0].y, t.p[1].x, t.p[1].y, t.p[2].x, t.p[2].y, t.sym, t.col);
                 DrawTriangle(t.p[0].x, t.p[0].y, t.p[1].x, t.p[1].y, t.p[2].x, t.p[2].y, PIXEL_SOLID, FG_WHITE);
             }
@@ -599,6 +604,134 @@ public:
 
         return true;
     }
+
+    void TexturedTriangle(int x1, int y1, float u1, float v1,
+        int x2, int y2, float u2, float v2,
+        int x3, int y3, float u3, float v3,
+        olcSprite* tex)
+    {
+        //low y to high y
+        if (y2 < y1)
+        {
+            std::swap(y1, y2);
+            std::swap(x1, x2);
+            std::swap(u1, u2);
+            std::swap(v1, v2);
+        }
+        if (y3 < y1)
+        {
+            std::swap(y1, y3);
+            std::swap(x1, x3);
+            std::swap(u1, u3);
+            std::swap(v1, v3);
+        }
+        if (y3 < y2)
+        {
+            std::swap(y2, y3);
+            std::swap(x2, x3);
+            std::swap(u2, u3);
+            std::swap(v2, v3);
+        }
+
+        int dy1 = y2 - y1; //one whole pixel, not half
+        int dx1 = x2 - x1;
+        float dv1 = v2 - v1;
+        float du1 = u2 - u1;
+
+        int dy2 = y3 - y1;
+        int dx2 = x3 - x1;
+        float dv2 = v3 - v1;
+        float du2 = u3 - u1;
+
+        float tex_u, tex_v;
+        float dax_step = 0;
+        float dbx_step = 0;
+        float du1_step = 0;
+        float dv1_step = 0;
+        float du2_step = 0;
+        float dv2_step = 0;
+        if (dy1) dax_step = dx1 / (float)abs(dy1);
+        if (dy2) dbx_step = dx2 / (float)abs(dy2);
+        if (dy1) du1_step = du1 / (float)abs(dy1);
+        if (dy1) dv1_step = dv1 / (float)abs(dy1);
+
+        //scan line fill
+        if (dy1)
+        {
+            for (int i = y1; i <= y2; i++)
+            {
+                int ax = x1 + (float)(i - y1) * dax_step;
+                int bx = x1 + (float)(i - y1) * dbx_step;
+
+                float tex_su = u1 + (float)(i - y1) * du1_step;
+                float tex_sv = v1 + (float)(i - y1) * dv1_step;
+                float tex_eu = u1 + (float)(i - y1) * du2_step;
+                float tex_ev = v1 + (float)(i - y1) * dv2_step;
+
+                if (ax > bx)
+                {
+                    std::swap(ax, bx);
+                    std::swap(tex_su, tex_eu);
+                    std::swap(tex_sv, tex_ev);
+                }
+                tex_u = tex_su;
+                tex_v = tex_sv;
+                float tstep = 1.0f / ((float)(bx - ax));
+                float t = 0.0f;
+                for (int j = ax; j < bx; j++)
+                {
+                    tex_u = (1.0f - t) * tex_su + t * tex_eu;
+                    tex_v = (1.0f - t) * tex_sv + t * tex_ev;
+                    Draw(j, i, tex->SampleGlyph(tex_u, tex_v), tex->SampleColour(tex_u, tex_v));
+
+                    t += tstep;
+                }
+            }
+
+            dy1 = y3 - y2;
+            dx1 = x3 - x2;
+            dy1 = v3 - v2;
+            du1 = u3 - u2;
+
+            if (dy1) dax_step = dx1 / (float)abs(dy1);
+            if (dy2) dbx_step = dx2 / (float)abs(dy2);
+            
+            du1_step, dv1_step = 0;
+            if (dy1) du1_step = du1 / (float)abs(dy1);
+            if (dy1) dv1_step = dv1 / (float)abs(dy1);
+
+            for (int i = y2; i <= y3; i++)
+            {
+                int ax = x2 + (float)(i - y2) * dax_step;
+                int bx = x1 + (float)(i - y2) * dbx_step;
+
+                float tex_su = u2 + (float)(i - y2) * du1_step;
+                float tex_sv = v2 + (float)(i - y2) * dv1_step;
+                float tex_eu = u1 + (float)(i - y1) * du2_step;
+                float tex_ev = v1 + (float)(i - y1) * dv2_step;
+
+                if (ax > bx)
+                {
+                    std::swap(ax, bx);
+                    std::swap(tex_su, tex_eu);
+                    std::swap(tex_sv, tex_ev);
+                }
+                tex_u = tex_su;
+                tex_v = tex_sv;
+                float tstep = 1.0f / ((float)(bx - ax));
+                float t = 0.0f;
+                for (int j = ax; j < bx; j++)
+                {
+                    tex_u = (1.0f - t) * tex_su + t * tex_eu;
+                    tex_v = (1.0f - t) * tex_sv + t * tex_ev;
+                    Draw(j, i, tex->SampleGlyph(tex_u, tex_v), tex->SampleColour(tex_u, tex_v));
+
+                    t += tstep;
+                }
+            }
+        }
+    }
+
 };
 
 int main()
